c6b614735f6aa2a04eb73bd5fef2be67bc9de1bc
Ianbcode <39637301+Ianbcode@users.noreply.github.com>
2023-06-09 16:23:17 -0700

    properly add selenium lib to referenced libraries

--- Java_Practice_Selenium/src/App.java
+++ Java_Practice_Selenium/src/App.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ian\\Documents\\GitHub\\Selenium_Practice\\Java_Practice_Selenium\\src\\drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Ian/Documents/GitHub/Selenium_Practice/Java_Practice_Selenium/src/drivers/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "C:/Users/kahraman.bayraktar/source/Repos/BOUNSWE/SWE550/demo/chromedriver.exe");
         // Create a new ChromeDriver instance
         WebDriver driver = new ChromeDriver();
 
